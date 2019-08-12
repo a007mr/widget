701bff5e13e12486326cc7d501f6e4469beb7608
a007mr <anton.cherkasov@gmail.com>
2019-08-12 12:54:42 +0300

    Update Referral.js

--- client/src/components/early-referrals/Referral.js
+++ client/src/components/early-referrals/Referral.js
@@ -3,13 +3,10 @@ import { render } from 'react-dom';
 import ReactDOM from 'react-dom';
 import RegisterReferral from './RegisterReferral.js';
 
+render(<RegisterReferral />, document.getElementById('referral'));
+
 // class App extends Component {}
 
 // render(<App />, document.getElementById('referral'));
-render(<RegisterReferral />, document.getElementById('referral'));
 
-// const Referral = () => {
-//   ReactDOM.render(<RegisterReferral />, document.getElementById('referral'));
-// };
 
-// export default Referral;
