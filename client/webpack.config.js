ac44c4ed66dd811b741f7befbeabbea0e0253421
a007mr <anton.cherkasov@gmail.com>
2019-08-12 12:53:48 +0300

    Update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,67 +1,3 @@
-// const webpack = require('webpack');
-// const path = require('path');
-// // const libraryName = 'library';
-// const widgetName = 'referral';
-// // const outputFile = widgetName + '.js';
-
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-// const env = process.env.WEBPACK_ENV;
-// const plugins = [];
-// let outputFile;
-// // let minimizer = [];
-
-// if (env === 'build') {
-//   // minimizer = new UglifyJsPlugin({ minimize: true });
-//   // plugins.push(new UglifyJsPlugin({ minimize: true }));
-//   // plugins.push(new config.optimization.minimize());
-//   outputFile = widgetName + '.min.js';
-// } else {
-//   outputFile = widgetName + '.js';
-// }
-
-// const isDevBuild = !(env && env.prod);
-
-// const config = {
-//   entry: __dirname + '/src/index.js',
-//   devtool: 'inline-source-map',
-//   output: {
-//     path: __dirname + '/widget',
-//     filename: outputFile
-//     // widget: widgetName,
-//     // widgetTarget: 'umd',
-//     // umdNamedDefine: true,
-//     // globalObject: "typeof self !== 'undefined' ? self : this"
-//   },
-//   plugins: isDevBuild
-//     ? [new webpack.SourceMapDevToolPlugin()]
-//     : [new webpack.optimize.UglifyJsPlugin()],
-//   module: {
-//     rules: [
-//       { test: /\.html$/i, use: 'html-loader' },
-//       {
-//         test: /\.css$/i,
-//         use: ['style-loader', 'css-loader' + (isDevBuild ? '' : '?minimize')]
-//       },
-//       {
-//         test: /(\\.jsx|\\.js)$/,
-//         loader: 'babel',
-//         exclude: /(node_modules|bower_components)/
-//       },
-//       {
-//         test: /(\\.jsx|\\.js)$/,
-//         loader: 'eslint-loader',
-//         exclude: /node_modules/
-//       }
-//     ]
-//   },
-//   resolve: {
-//     modules: [path.resolve('./node_modules'), path.resolve('./src')],
-//     extensions: ['.json', '.js']
-//   }
-// };
-
-// module.exports = config;
-
 const path = require('path');
 const webpack = require('webpack');
 var copyWebpackPlugin = require('copy-webpack-plugin');
@@ -80,7 +16,6 @@ module.exports = env => {
   return [
     {
       mode: mode,
-      // entry: './src/index.js',
       entry: './src/components/early-referrals/Referral.js',
       output: {
         filename: 'widget.js',
@@ -140,11 +75,6 @@ module.exports = env => {
               }
             }
           }
-          // {
-          //   test: /(\.jsx|\.js)$/,
-          //   loader: 'eslint-loader',
-          //   exclude: /node_modules/
-          // }
         ]
       },
       resolve: {
